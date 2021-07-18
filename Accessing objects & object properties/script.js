30567d95a53b29b7d2b77f1e8f77dda367b4a2b7
Dhanith Wickremaratne <59153767+DhanithLakjaya@users.noreply.github.com>
2021-07-18 23:51:12 +0530

    Objects

---
+++ Accessing objects & object properties/script.js
@@ -0,0 +1,41 @@
+const admin = {
+
+    name: "Dhanith Lakjaya",
+    age: 23,
+
+    results: {
+
+        Java: "A+",
+        ADMS: "A",
+    },
+
+};
+
+// accessing objects(output in console)
+
+console.log("The admin object: ", admin);
+
+
+
+
+//two ways of accessing object properties
+//1 dot notation
+//easy to read and understand
+
+
+console.log("The Age value: ", admin.age);
+console.log("Exam results ADMS:", admin.results.ADMS);
+
+
+//2 bracket notation
+//more control
+
+
+
+
+
+
+
+
+
+
